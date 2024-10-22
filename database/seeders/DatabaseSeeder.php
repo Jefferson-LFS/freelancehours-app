7a56c754e61c04b55a8ff94f490ab0fd90013446
Jefferson Lucas <jefferson.altere@gmail.com>
2024-10-21 23:28:02 -0300

    Atualiza ranking das propostas

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Actions\ArrangePositions;
 use App\Models\Project;
 use App\Models\Proposal;
 use App\Models\User;
@@ -25,6 +26,8 @@ class DatabaseSeeder extends Seeder
 
 
                 Proposal::factory()->count(random_int(4, 45))->create(['project_id' => $project->id]);
+
+              ArrangePositions::run($project->id);
 
         });
 
